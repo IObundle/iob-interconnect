1f475d96199cb55e9c68102f82765c4f64fad08d
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-06-26 01:13:07 +0100

    improve hardware settings file

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,4 +1,12 @@
-INTERCON_SRC_DIR:=$(INTERCON_DIR)/hardware/src
+INTERCON_HW_DIR:=$(INTERCON_DIR)/hardware
+
+#include
+INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
+INCLUDE+=$(incdir) $(INTERCON_INC_DIR)
+
+#headers
+VHDR+=$(INTERCON_INC_DIR)/*.vh
+
+#sources
+INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
 VSRC+= $(INTERCON_SRC_DIR)/merge.v $(INTERCON_SRC_DIR)/split.v
-
-

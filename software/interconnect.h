a6f8a54f2994698d473bfd0822cca918c8a0a030
Chris Edgley <christopher@iobundle.com>
2020-07-15 18:37:29 +0100

    name change RAM_->MEM_; MEM->IO_

--- software/interconnect.h
+++ software/interconnect.h
@@ -1,9 +1,9 @@
 //memory access macros
-#define RAM_GET(type, location, value) (*((type*) (location)))
-#define RAM_SET(type, location, value) (*((type*) (location)) = value)
+#define MEM_GET(type, location, value) (*((type*) (location)))
+#define MEM_SET(type, location, value) (*((type*) (location)) = value)
 
 //stream access macros
-#define MEMSET(base, location, value) (*((volatile int*) (base + (sizeof(int)) * location)) = value)
-#define MEMGET(base, location)        (*((volatile int*) (base + (sizeof(int)) * location)))
+#define IO_SET(base, location, value) (*((volatile int*) (base + (sizeof(int)) * location)) = value)
+#define IO_GET(base, location)        (*((volatile int*) (base + (sizeof(int)) * location)))
 
 

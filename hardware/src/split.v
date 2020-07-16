03cf19904606669e558eb43db08dfe2da4d76b3d
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-07-16 17:28:18 +0100

    simply split

--- hardware/src/split.v
+++ hardware/src/split.v
@@ -21,29 +21,20 @@ module split
     input [N_SLAVES*`RESP_W-1:0]     s_resp
     );
    
-   //mask select bit MSB
+   //slave select word
    wire [`Nb:0]                      s_sel_int = m_req[P_SLAVES+1:P_SLAVES+1-`Nb] & {1'b0, {`Nb{1'b1}}};
    
-                             
-   //deliver request to selected slave
+   //route master and slave                       
    integer                        i;
    always @* begin
-      for (i=0; i<N_SLAVES; i=i+1)
-        if(i == s_sel_int)
-          //delete master request to slave
-          s_req[`req(i)] = m_req;
-        else
-          //delete slave request
-          s_req[`req(i)] = {(`REQ_W){1'b0}};
-   end
-
-   //route selected slave response to master
-   integer                       j;
-   always @* begin
       m_resp = {(`RESP_W){1'b0}};
-      for (j=0; j<N_SLAVES; j=j+1)
-        if(j == s_sel_int )
-          m_resp = s_resp[`resp(j)];
+      for (i=0; i<N_SLAVES; i=i+1)
+        if(i == s_sel_int) begin
+           s_req[`req(i)] = m_req;
+           m_resp = s_resp[`resp(i)];
+       end else begin
+         s_req[`req(i)] = {(`REQ_W){1'b0}};
+       end
    end
    
 endmodule

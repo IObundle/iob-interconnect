ee687dd7b912a7564931a164b46b9f5b2355f062
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-08-17 13:41:07 +0100

    updated for pipeline cpu operation

--- hardware/src/merge.v
+++ hardware/src/merge.v
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ps
 `include "interconnect.vh"
 
+`define Nb $clog2(N_MASTERS)+($clog2(N_MASTERS)==0)
+
 module merge
   #(
     parameter N_MASTERS = 2,
@@ -8,6 +10,10 @@ module merge
     parameter ADDR_W = 32
     )
    (
+
+    input                              clk,
+    input                              rst,
+
     //masters interface
     input [N_MASTERS*`REQ_W-1:0]       m_req,
     output reg [N_MASTERS*`RESP_W-1:0] m_resp,
@@ -17,18 +23,52 @@ module merge
     input [`RESP_W-1:0]                s_resp
     );
 
-   integer                          i;
-
+   //                               
    //priority encoder: most significant bus has priority   
+   //
+   reg [`Nb-1:0]                      sel, sel_reg;
+
+   //select master
+   integer                             k;
    always @* begin
-      s_req = {`REQ_W{1'b0}};
-      m_resp = {N_MASTERS*`RESP_W{1'b0}};
-      for (i=0; i<N_MASTERS; i=i+1)
-        if(m_req[`valid(i)]) begin //test valid bit 
-           s_req = m_req[`req(i)];
-           m_resp = {N_MASTERS*`RESP_W{1'b0}};
-           m_resp[`resp(i)] = s_resp;
-        end
+      sel = {`Nb{1'b0}};
+      for (k=0; k<N_MASTERS; k=k+1)
+        if( m_req[`valid(k)] )
+          sel = k[`Nb-1:0];          
    end
 
+   //
+   //route master request to slave
+   //  
+   integer                             i;
+   always @* begin
+      s_req = {`REQ_W{1'b0}};
+      for (i=0; i<N_MASTERS; i=i+1)
+        if( i == sel )
+          s_req = m_req[`req(i)];
+   end
+
+   //
+   //route response from slave to previously selected master
+   //
+
+   //register master selection
+   always @( posedge clk, posedge rst ) begin
+      if( rst )
+        sel_reg <= {`Nb{1'b0}};
+      else
+        sel_reg <= sel;
+   end
+   
+   //route
+   integer                             j;
+   always @* begin
+      for (j=0; j<N_MASTERS; j=j+1)
+        if( j == sel )
+          m_resp[`resp(j)] = s_resp;
+        else
+          m_resp[`resp(j)] = {`RESP_W{1'b0}};
+   end
+
+ 
 endmodule
--- hardware/src/split.v
+++ hardware/src/split.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 `include "interconnect.vh"
 
-`define Nb $clog2(N_SLAVES)
+`define Nb $clog2(N_SLAVES)+($clog2(N_SLAVES)==0)
 
 module split
   #(
@@ -11,30 +11,56 @@ module split
     parameter P_SLAVES = `REQ_W-2 //slave select word msb position
     )
    (
+    input                            clk,
+    input                            rst,
+  
     //masters interface
     input [`REQ_W-1:0]               m_req,
     output reg [`RESP_W-1:0]         m_resp,
 
     //slave interface
-    //input [$clog2(N_SLAVES):0]       s_sel,
     output reg [N_SLAVES*`REQ_W-1:0] s_req,
     input [N_SLAVES*`RESP_W-1:0]     s_resp
     );
    
    //slave select word
-   wire [`Nb:0]                      s_sel_int = m_req[P_SLAVES+1:P_SLAVES+1-`Nb] & ({`Nb+1{1'b1}}>>1);
+   wire [`Nb:0]                      s_sel = m_req[P_SLAVES+1 -:`Nb+1] & ({(`Nb+1){1'b1}}>>1);
    
-   //route master and slave                       
-   integer                        i;
+   //route master request to selected slave
+   integer                           i;
    always @* begin
-      m_resp = {(`RESP_W){1'b0}};
-      for (i=0; i<N_SLAVES; i=i+1)
-        if(i == s_sel_int) begin
-           s_req[`req(i)] = m_req;
-           m_resp = s_resp[`resp(i)];
-       end else begin
+      /*
+     $display("pslave %d", P_SLAVES+1);
+     $display("mreq %x", m_req);
+     $display("s_sel %x", s_sel);
+   */
+     for (i=0; i<N_SLAVES; i=i+1)
+       if(i == s_sel)
+         s_req[`req(i)] = m_req;
+       else
          s_req[`req(i)] = {(`REQ_W){1'b0}};
-       end
+   end
+   
+   //
+   //route response from previously selected slave to master
+   //
+
+   //register the slave selection
+   reg [`Nb:0]                       s_sel_reg;
+   always @( posedge clk, posedge rst ) begin
+      if( rst )
+        s_sel_reg <= {`Nb{1'b0}};
+      else
+        s_sel_reg <= s_sel;          
+   end
+   
+   //route
+   integer                           j;
+   always @* begin
+      m_resp = {`RESP_W{1'b0}};
+      for (j=0; j<N_SLAVES; j=j+1)
+        if( j == s_sel_reg )
+          m_resp = s_resp[`resp(j)];
    end
    
 endmodule

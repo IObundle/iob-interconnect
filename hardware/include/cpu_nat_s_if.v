18051f684579da4c5cac5a21a1728f21f91969b8
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-10-05 01:45:33 +0100

    replace hardcoded value

--- hardware/include/cpu_nat_s_if.v
+++ hardware/include/cpu_nat_s_if.v
@@ -2,6 +2,6 @@
                `INPUT(valid,   1),  //Native CPU interface valid signal
 	       `INPUT(address, ADDR_W),  //Native CPU interface address signal
                `INPUT(wdata,   WDATA_W), //Native CPU interface data write signal
-	       `INPUT(wstrb,   1),  //Native CPU interface write strobe signal
+	       `INPUT(wstrb,   DATA_W/8),  //Native CPU interface write strobe signal
 	       `OUTPUT(rdata,  DATA_W), //Native CPU interface read data signal
 	       `OUTPUT(ready,  1),  //Native CPU interface ready signal

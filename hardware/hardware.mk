f6ebd2af3a27ac3b78504bb10da15cab08864be0
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-11-15 02:03:28 +0000

    config.mk and hardware.mk

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,13 +1,18 @@
-#path
+include $(INTERCON_DIR)/config.mk
+
+#add itself to MODULES list
+MODULES+=$(MODULE)
+
+#paths
 INTERCON_HW_DIR:=$(INTERCON_DIR)/hardware
+INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
+INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
 
 #include
-INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
 INCLUDE+=$(incdir) $(INTERCON_INC_DIR)
 
 #headers
 VHDR+=$(INTERCON_INC_DIR)/*.vh $(INTERCON_DIR)/hardware/include/*.v
 
 #sources
-INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
 VSRC+= $(INTERCON_SRC_DIR)/merge.v $(INTERCON_SRC_DIR)/split.v

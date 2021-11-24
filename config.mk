223ae468694bdea5dcfa1f68af3ba5515f024e82
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-11-24 10:43:09 +0000

    remove spaces between incdir variable and paths; use path variables instead hardcoded paths; move paths to config.mk

--- config.mk
+++ config.mk
@@ -1,3 +1,8 @@
 MODULE=INTERCON
 
+#paths
+INTERCON_HW_DIR:=$(INTERCON_DIR)/hardware
+INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
+INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
+
 SIM_DIR ?=hardware/simulation/icarus
--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -3,16 +3,11 @@ include $(INTERCON_DIR)/config.mk
 #add itself to MODULES list
 MODULES+=INTERCON
 
-#paths
-INTERCON_HW_DIR:=$(INTERCON_DIR)/hardware
-INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
-INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
-
 #include
-INCLUDE+=$(incdir) $(INTERCON_INC_DIR)
+INCLUDE+=$(incdir)$(INTERCON_INC_DIR)
 
 #headers
-VHDR+=$(INTERCON_INC_DIR)/*.vh $(INTERCON_DIR)/hardware/include/*.v
+VHDR+=$(INTERCON_INC_DIR)/*.vh $(INTERCON_INC_DIR)/*.v
 
 #sources
 VSRC+= $(INTERCON_SRC_DIR)/merge.v $(INTERCON_SRC_DIR)/split.v

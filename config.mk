75ff3041f9052e93e3d6b3b7781592e8b6aa793b
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-11-24 16:41:58 +0000

    reorganize simulation Makefile; add INTERCON paths in config.mk; rename clean to sim-clean

--- config.mk
+++ config.mk
@@ -2,5 +2,13 @@
 INTERCON_HW_DIR:=$(INTERCON_DIR)/hardware
 INTERCON_INC_DIR:=$(INTERCON_HW_DIR)/include
 INTERCON_SRC_DIR:=$(INTERCON_HW_DIR)/src
+INTERCON_TB_DIR:=$(INTERCON_HW_DIR)/testbench
+INTERCON_SIM_DIR:=$(INTERCON_HW_DIR)/simulation
+SUBMODULES_DIR:=$(INTERCON_DIR)/submodules
 
-SIM_DIR ?=hardware/simulation/icarus
+SIM_DIR ?=$(INTERCON_HW_DIR)/simulation/icarus
+
+# SUBMODULE PATHS
+SUBMODULES=
+SUBMODULE_DIRS=$(shell ls $(SUBMODULES_DIR))
+$(foreach d, $(SUBMODULE_DIRS), $(eval TMP=$(shell make -C $(SUBMODULES_DIR)/$d corename | grep -v make)) $(eval SUBMODULES+=$(TMP)) $(eval $(TMP)_DIR ?=$(SUBMODULES_DIR)/$d))
